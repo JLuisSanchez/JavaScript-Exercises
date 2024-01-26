ea281f94e625065afa9e230da06dd0edfc7c7913
Jose Luis Sanchez Gutierrez <99083965+JLuisSanchez@users.noreply.github.com>
2024-01-26 10:18:39 -0600

    Add files via upload

---
+++ Juego_Numero_Secreto/app.js
@@ -0,0 +1,36 @@
+//Variables
+let numeroMaximoPosible = prompt("Indica la cantidad de números entre los que quieres adivinar (p.e. 20, 50, 100)");
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
+let numeroUsuario = 0;
+let intentos = 1;
+console.log (numeroSecreto)
+let maximosIntentos = prompt("Indica la cantidad de intentos en las que quieres adivinar el número secreto");
+
+
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = parseInt(prompt(`Me indicas un número entre 1 y ${numeroMaximoPosible} por favor:`));
+
+    console.log(typeof(numeroUsuario));
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('El número secreto es menor');
+        } else {
+            alert('El número secreto es mayor');
+        }
+        //Incrementamos el contador cuando no acierta
+        //intentos = intentos + 1;
+        //intentos += 1;
+        intentos++;
+
+        //palabraVeces = 'veces';
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
+            break;
+        }
+        //La condición no se cumplió
+        //alert('Lo siento, no acertaste el número');
+    }
+}
